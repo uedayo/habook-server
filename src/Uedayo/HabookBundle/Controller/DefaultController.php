5b5f9e591f0285889a64c543e1e7b07ce98c9e2c
Yoshinori Ueda <uedayo@gmail.com>
2013-06-04 15:29:21 +0900

    modify DefaultController

--- src/Uedayo/HabookBundle/Controller/DefaultController.php
+++ src/Uedayo/HabookBundle/Controller/DefaultController.php
@@ -7,9 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
+
+    public function indexAction()
     {
-        return $this->render('UedayoHabookBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getEntityManager();
+        $posts = $em->getRepository('UedayoHabookBundle:Book')->findAll();
+        return $this->render('UedayoHabookBundle:Default:index.html.twig', array('posts' => $posts));
+    }
+
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $post = $em->find('UedayoHabookBundle:Default:Book', $id);
+        return $this->render('UedayoHabookBundle:Default:index.html.twig', array('post' => $post));
     }
 }
